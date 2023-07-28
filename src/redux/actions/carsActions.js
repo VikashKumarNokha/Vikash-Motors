680195eb55a52e5d3f9139d20767dd415a70f7e2
Vikash Kumar <87219068+VikashKumarNokha@users.noreply.github.com>
2023-07-28 17:30:50 +0530

    Update carsActions.js

--- src/redux/actions/carsActions.js
+++ src/redux/actions/carsActions.js
@@ -7,7 +7,7 @@ export const getAllCars=()=>async dispatch=>{
     dispatch({type: 'LOADING' , payload:true})
 
     try {
-        const response = await axios.get('https://adrenturebackend.herokuapp.com/api/cars/getallcars')
+        const response = await axios.get('https://vikash-motors-backend.onrender.com/api/cars/getallcars')
         //console.log(response.data)
         dispatch({type: 'GET_ALL_CARS', payload:response.data})
         dispatch({type: 'LOADING' , payload:false})
